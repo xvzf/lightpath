c16d07ec4a235cc5864c27b87b509167cba11eff
Matthias Riegler <me@xvzf.tech>
2022-05-13 18:49:04 +0200

    chore: move to state from internal to pkg
    Signed-off-by: Matthias Riegler <me@xvzf.tech>

--- cmd/controlplane/controlplane.go
+++ cmd/controlplane/controlplane.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"time"
 
-	"github.com/xvzf/lightpath/internal/lightpath/state"
+	"github.com/xvzf/lightpath/pkg/state"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
